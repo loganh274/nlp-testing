e395c3a22360cc69a9e2bd4646ec3c8cc03e1f00
loganh274 <104328515+loganh274@users.noreply.github.com>
2025-12-01 22:40:12 +1300

    importing libraries

--- nlp.py
+++ nlp.py
@@ -5,6 +5,10 @@ import matplotlib.pyplot as plt
 from sklearn.linear_model import LogisticRegression
 from sklearn.feature_extraction.text import CountVectorizer
 
+import gensim.downloader as api
+
+
+
 
 negative_table = pd.read_csv('negative.csv')
 positive_table = pd.read_csv('positive.csv')
@@ -12,5 +16,7 @@ positive_table = pd.read_csv('positive.csv')
 negative = negative_table.iloc[:, 4].tolist()
 positive = positive_table.iloc[:, 4].tolist()
 
-print(len(negative))
-print(len(positive))
+corpus = negative + positive
+labels = [0] * len(negative) + [1] * len(positive)
+
+print(labels)
